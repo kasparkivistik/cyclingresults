a739c8e3f1f868af911f80ec41247fe1f13506f3
kasparkivistik <kkivis@ttu.ee>
2018-04-07 23:47:47 +0300

    Initial commit

---
+++ src/main/java/ee/ttu/idu0075/cyclingresults/CyclingresultsApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.idu0075.cyclingresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingresultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclingresultsApplication.class, args);
+	}
+}
